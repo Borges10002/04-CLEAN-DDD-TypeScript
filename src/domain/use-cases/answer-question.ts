82209736eebcba529a9e4027f7de33e474a3b13c
Borges10002 <borges10002@gmail.com>
2024-04-17 20:37:02 -0400

    update

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -8,7 +8,7 @@ interface answerQuestionUseCaseRequest {
 
 export class AnswerQuestionUseCase {
   execute({ instructorId, questionId, content }: answerQuestionUseCaseRequest) {
-    const answer = new Answer(content);
+    const answer = new Answer({ content, authorId: instructorId, questionId });
 
     return answer;
   }

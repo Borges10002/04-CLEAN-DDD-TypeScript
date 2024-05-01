7f2c5421150d8c4a5f6627ab9160cfb4356108ef
Borges10002 <borges10002@gmail.com>
2024-05-01 13:32:38 -0400

    create class value object de slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -3,18 +3,22 @@ import { randomUUID } from "node:crypto";
 interface QuestionProps {
   title: string;
   content: string;
+  slug: string;
   authorId: string;
 }
 
 class Question {
   public id: string;
   public title: string;
+  public slug: string;
   public content: string;
+
   public authorId: string;
 
   constructor(props: QuestionProps, id?: string) {
     this.title = props.title;
     this.content = props.content;
+    this.slug = props.slug;
     this.authorId = props.authorId;
     this.id = id ?? randomUUID();
   }

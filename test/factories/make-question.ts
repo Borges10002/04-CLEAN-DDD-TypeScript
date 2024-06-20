a5d9e63e42b0a61ac07cdaf53b13f28573200524
Borges10002 <borges10002@gmail.com>
2024-06-19 21:40:08 -0400

    update

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -4,15 +4,22 @@ import {
   QuestionProps,
 } from "@/domain/forum/interprise/entities/question";
 import { Slug } from "@/domain/forum/interprise/entities/value-objects/slug";
+import { faker } from "@faker-js/faker";
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: "Example question",
-    slug: Slug.create("Example question"),
-    content: "Example question",
-    ...override,
-  });
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      slug: Slug.create("Example question"),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  );
 
   return question;
 }

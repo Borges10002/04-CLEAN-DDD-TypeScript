42649baeef1470a29da08656523296ff54751852
Borges10002 <borges10002@gmail.com>
2024-07-10 21:14:18 -0400

    update

---
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -0,0 +1,38 @@
+import { Answer } from "../../interprise/entities/answer";
+import { AnswersRepository } from "../repositories/answers-repository";
+
+interface EditAnswerUseCaseRequest {
+  authorId: string;
+  answerId: string;
+  content: string;
+}
+
+interface EditAnswerUseCaseResponse {
+  answer: Answer;
+}
+
+export class EditAnswerUseCase {
+  constructor(private answersRepository: AnswersRepository) {}
+
+  async execute({
+    authorId,
+    answerId,
+    content,
+  }: EditAnswerUseCaseRequest): Promise<EditAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId);
+
+    if (!answer) {
+      throw new Error("Question not found.");
+    }
+
+    if (authorId !== answer.authorId.toString()) {
+      throw new Error("Not allowed.");
+    }
+
+    answer.content = content;
+
+    await this.answersRepository.save(answer);
+
+    return { answer };
+  }
+}

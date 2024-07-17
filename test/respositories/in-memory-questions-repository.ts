1f0306146ff7058a1debf3cd228edd14476a503b
Borges10002 <borges10002@gmail.com>
2024-07-16 20:50:25 -0400

    case use list question

--- test/respositories/in-memory-questions-repository.ts
+++ test/respositories/in-memory-questions-repository.ts
@@ -1,7 +1,10 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
 import { Question } from "@/domain/forum/interprise/entities/question";
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
+  public items: Question[] = [];
+
   async findById(id: string) {
     const question = this.items.find((item) => item.id.toString() === id);
 
@@ -22,7 +25,13 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     return question;
   }
 
-  public items: Question[] = [];
+  async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20);
+
+    return questions;
+  }
 
   async create(question: Question) {
     this.items.push(question);

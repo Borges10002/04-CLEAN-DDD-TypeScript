c94875ec6a3631fbafa4266c2f1676c260888949
Borges10002 <borges10002@gmail.com>
2024-07-30 21:46:49 -0400

    update

--- src/domain/forum/interprise/entities/question-comment.ts
+++ src/domain/forum/interprise/entities/question-comment.ts
@@ -1,50 +1,21 @@
-import { Entity } from "@/core/entities/entity";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Optional } from "@/core/types/optional";
+import { Comment, CommentProps } from "./comment";
 
-export interface QuestionComment {
-  authorId: UniqueEntityID;
-  answerId: UniqueEntityID;
-  content: string;
-  createdAt: Date;
-  updatedAt?: Date;
+export interface QuestionPropsProps extends CommentProps {
+  questionId: UniqueEntityID;
 }
 
-export class AnswerComment extends Entity<QuestionComment> {
-  get authorId() {
-    return this.props.authorId;
-  }
-
-  get answerId() {
-    return this.props.answerId;
-  }
-
-  get content() {
-    return this.props.content;
-  }
-
-  get createdAt() {
-    return this.props.createdAt;
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt;
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date();
-  }
-
-  set content(content: string) {
-    this.props.content = content;
-    this.touch();
+export class QuestionProps extends Comment<QuestionPropsProps> {
+  get questionId() {
+    return this.props.questionId;
   }
 
   static create(
-    props: Optional<QuestionComment, "createdAt">,
+    props: Optional<QuestionPropsProps, "createdAt">,
     id?: UniqueEntityID,
   ) {
-    const answerComment = new AnswerComment(
+    const questionProps = new QuestionProps(
       {
         ...props,
         createdAt: props.createdAt ?? new Date(),
@@ -52,6 +23,6 @@ export class AnswerComment extends Entity<QuestionComment> {
       id,
     );
 
-    return answerComment;
+    return questionProps;
   }
 }

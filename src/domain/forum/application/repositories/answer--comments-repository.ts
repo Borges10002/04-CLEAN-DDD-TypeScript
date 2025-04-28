c2194931c9afda11268894510b71f43272a667b7
Borges10002 <borges10002@gmail.com>
2025-04-28 18:51:49 -0400

    case of use list comment

--- src/domain/forum/application/repositories/answer--comments-repository.ts
+++ src/domain/forum/application/repositories/answer--comments-repository.ts
@@ -1,5 +1,7 @@
 import { AnswerComment } from "../../interprise/entities/answer-commen";
 
 export interface AnswerCommentsRepository {
+  findById(id: string): Promise<AnswerComment | null>;
   create(questionComment: AnswerComment): Promise<void>;
+  delete(answerComment: AnswerComment): Promise<void>;
 }

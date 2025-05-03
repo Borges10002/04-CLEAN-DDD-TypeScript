d385b7f88dbb021f9521d12133f86d2f5fb8733e
Borges10002 <borges10002@gmail.com>
2025-05-02 21:43:12 -0400

    sped

--- src/domain/forum/application/repositories/answer--comments-repository.ts
+++ src/domain/forum/application/repositories/answer--comments-repository.ts
@@ -1,7 +1,13 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswerComment } from "../../interprise/entities/answer-commen";
 
 export interface AnswerCommentsRepository {
   findById(id: string): Promise<AnswerComment | null>;
+  findManyByAnswerId(
+    answerId: string,
+    params: PaginationParams,
+  ): Promise<AnswerComment[]>;
+
   create(questionComment: AnswerComment): Promise<void>;
   delete(answerComment: AnswerComment): Promise<void>;
 }

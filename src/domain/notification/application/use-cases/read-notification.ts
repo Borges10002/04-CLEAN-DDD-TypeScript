6ebe6fb773f6a7d81fc02222564b590e374535c4
Borges10002 <borges10002@gmail.com>
2025-07-10 21:01:00 -0400

    notification

---
+++ src/domain/notification/application/use-cases/read-notification.ts
@@ -0,0 +1,43 @@
+import { Either, left, right } from "@/core/either";
+import { Notification } from "../../enterprise/entities/notification";
+import { ResourceNotFoundError } from "@/domain/forum/application/use-cases/errors/resource-not-found-error";
+import { NotAllowedError } from "@/domain/forum/application/use-cases/errors/not-allowed-error";
+import { NotificationsRepository } from "../respositories/notication-repository";
+
+interface ReadNotificationUseCaseRequest {
+  recipientId: string;
+  notificationId: string;
+}
+
+type ReadNotificationUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {
+    notification: Notification;
+  }
+>;
+
+export class ReadNotificationUseCase {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute({
+    recipientId,
+    notificationId,
+  }: ReadNotificationUseCaseRequest): Promise<ReadNotificationUseCaseResponse> {
+    const notification =
+      await this.notificationsRepository.findById(notificationId);
+
+    if (!notification) {
+      return left(new ResourceNotFoundError());
+    }
+
+    if (recipientId !== notification.recipientId.toString()) {
+      return left(new NotAllowedError());
+    }
+
+    notification.read();
+
+    await this.notificationsRepository.save(notification);
+
+    return right({ notification });
+  }
+}
